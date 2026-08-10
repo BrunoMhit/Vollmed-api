d6b9f4a1fdd4e14337d22baae68a6b76dd7ba5a9
Bruno Henrique <brunowhenrique1@gmail.com>
2026-08-09 22:13:40 -0300

    Add user entity and mysql migration

---
+++ src/main/java/med/voll/web_application/domain/usuario/Usuario.java
@@ -0,0 +1,35 @@
+package med.voll.web_application.domain.usuario;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name="usuarios")
+public class Usuario implements UserDetails {
+    //atributos da classe
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String senha;
+    private String nome;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}
